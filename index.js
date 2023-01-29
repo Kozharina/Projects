7cdec3db66f0961c622d5aa01b5a8ff1e63dd458
Kozharina <120336880+Kozharina@users.noreply.github.com>
2023-01-29 22:53:46 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,112 @@
+
+// Задание #1 
+/*
+let password = 'пароль';
+const enteredPassword = prompt('Введите пароль');
+ if(password === enteredPassword) {
+    alert("Пароль введен верно");
+ }
+ else {
+  alert("Пароль введен неправильно")
+ }
+*/
+
+// Задание #2
+
+/*
+const c = prompt('Введите чиcло от 0 до 10');
+const result = Number(c > 0 && c < 10) ? 'Верно': 'Неверно';
+console.log(result);
+*/
+
+// Задание #3
+
+/*
+const d = 1;
+const e = 37;
+
+if (d > 100 || e > 100) {
+    alert('Верно');
+} else {
+    alert('Неверно');
+}
+*/
+
+// Задание #4
+/*
+let a = Number('2');
+let b = Number('3');
+alert(a + b);
+*/
+
+// Задание #5 
+
+/*
+let monthNumber = prompt('Введите месяц');
+
+switch (monthNumber) {
+        case '1':
+        case '1':
+        case '12':
+          alert( 'Зима' );
+          break;
+
+        case '3':
+        case '4':
+        case '5':
+           alert('Весна');
+           break;
+
+        case '6':
+        case '7':
+        case '8':
+            alert('Лето');
+            break;
+
+        case '9':
+        case '10':
+        case '11':
+            alert('Осень');
+            break;
+        default:
+          alert('номер месяца больше 13');
+      }
+      */
+
+// Задание #6
+
+// https://github.com/Kozharina/Projects/pull/4
+
+// папка homework
+
+// Задание 7
+
+/*
+let check = prompt('Введите число')
+
+if( !isNaN(check) ) {
+ if( check % 2 === 0) {
+  alert(`Число ${check} четное`);
+ } 
+ else { 
+  alert(`Число ${check} нечетное`);
+}
+}
+else {
+  alert ("Вы ввели не число");
+}
+*/
+
+// Задание #8 
+/*
+let clientOS = +prompt ('Ведите Операционную систему 0 — iOS, 1 — Android');
+
+switch(clientOS) {
+  case 0: alert("Установите версию приложения для iOS по ссылке");break;
+  case 1: alert("Установите версию приложения для Android по ссылке");break;
+  default:alert("Введите вашу операционную систему: iOS или Android")
+}
+*/
+
+// Задание #9
+
